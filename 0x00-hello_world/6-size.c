861f7d479aa6412f70b39c96ff5de42fb3e4c180
Marwan <wana2810@yahoo.com>
2023-09-08 13:16:05 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -12,6 +11,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
 	printf("Size of a float: %ld byte(s)\n",sizeof(float));
-
 	return(0);
 }
