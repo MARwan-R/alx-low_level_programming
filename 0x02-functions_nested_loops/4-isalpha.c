c1c304b2f8f133036102bc81f8947f199c91e62d
Marwan <wana2810@yahoo.com>
2023-09-13 14:51:33 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Test the char if alphabet
+ *
+ * @c: Number to pass to _islower function
+ *
+ * Return: 1 if char, 0 if else
+*/
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 89)
+		return (1);
+	else
+		return (0);
+}
