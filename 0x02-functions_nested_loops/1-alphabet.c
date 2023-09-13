464a9afba160fab93ef26bce03b72e3394cb51e1
Marwan <wana2810@yahoo.com>
2023-09-13 13:27:30 -0700

    insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,11 @@
 /**
  * main - Entry point
  *
- * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ * Description: print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 void print_alphabet(void)
 {
 	int ch;
