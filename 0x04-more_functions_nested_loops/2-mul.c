919c39320b4ff06395365c74f4d9daf3853e253c
Marwan <wana2810@yahoo.com>
2023-09-14 16:49:38 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply input
+ *
+ * @a: frist num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
