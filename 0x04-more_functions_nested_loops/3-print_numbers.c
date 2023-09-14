280da2a250dbc6131ee2b9f3a24209c0746dfbf1
Marwan <wana2810@yahoo.com>
2023-09-14 16:57:12 -0700

    nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_numbers - Prints the nums followed by a new line.
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; ch <= '9'; num++)
+		_putchar(num);
+	_putchar('\n');
+}
