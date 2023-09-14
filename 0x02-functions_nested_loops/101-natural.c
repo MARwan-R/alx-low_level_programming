532e8e44423c8f1ae8633ec9c069cc3156efcf1c
Marwan <wana2810@yahoo.com>
2023-09-14 14:07:21 -0700

    mm

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
