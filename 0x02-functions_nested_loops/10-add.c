8c2270493ab1f797531b7e90af1ae3c313385c07
Marwan <wana2810@yahoo.com>
2023-09-13 18:20:07 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds input
+ *
+ * @a: frist num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
