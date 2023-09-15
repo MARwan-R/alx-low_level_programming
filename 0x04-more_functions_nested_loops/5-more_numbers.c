4d1b8157c5281e5d8d3720c47859f0ab35afaf2f
Marwan <wana2810@yahoo.com>
2023-09-14 17:34:18 -0700

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints from 0 to 14 ten times
+*/
+void more_numbers(void)
+{
+	int ch, num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (ch = 0; ch <= 14; ch++)
+			_putchar('0' + ch);
+		_putchar('\n');
+	}
+}
