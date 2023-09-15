38bd273db05c101629cf3375c75202c7e61b444b
Marwan <wana2810@yahoo.com>
2023-09-14 17:52:46 -0700

    more nums

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,20 @@
 */
 void more_numbers(void)
 {
-	int ch, num;
+	int ch, num, count;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (ch = 0; ch <= 14; ch++)
-			_putchar(ch);
+		{
+			count = ch;
+			if (ch > 9)
+			{
+				_putchar(1 + 48);
+				count = ch % 10;
+			}
+			_putchar(count + 48);
+		}
 		_putchar('\n');
 	}
 }
