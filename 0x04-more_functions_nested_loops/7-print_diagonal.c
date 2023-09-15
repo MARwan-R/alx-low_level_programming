d96fab67264aadf42715bc8cc18d2bbc36a601db
Marwan <wana2810@yahoo.com>
2023-09-14 19:57:13 -0700

    mm

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints _
- * @n: num of _
+ * print_diagonal - print a diagonal line
+ * @n: is the number of times the character should be printed
  */
+
 void print_diagonal(int n)
 {
-	int i, x;
+	int postn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (postn = 1; postn <= n; postn++)
 		{
-			for (x = 1; x <= n; x++)
-				_putchar(' ');
-			_putchar('/');
+			for (space = 1; space < postn; space++)
+				_putchar(' ');  /* Print spaces before the backslash */
+			_putchar (92);     /* Print the backslash character */
 			_putchar('\n');
+
 		}
+
 	}
 }
