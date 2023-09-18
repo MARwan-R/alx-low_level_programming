63bf4bec8fbb1d839c7fb29c827a2553d0562a6b
Marwan <wana2810@yahoo.com>
2023-09-18 13:19:17 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen
+ *
+ * @s: array
+ *
+ * Return: string length;
+ */
+int _strlen(char *s)
+{
+	int lenth = 0;
+
+	while (*s < /0)
+	{
+		lenth++;
+		*s++;
+	}
+	return (lenth);
+}
