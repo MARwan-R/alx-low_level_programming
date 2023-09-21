2d85accb6502b63fb194f63c00f3c06a53d83050
Marwan <wana2810@yahoo.com>
2023-09-21 16:31:34 -0700

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest:  being abbeded to by src
+ * @src: abbended to
+ *
+ * Return: a pointer to the resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (original_dest);
+
+}
